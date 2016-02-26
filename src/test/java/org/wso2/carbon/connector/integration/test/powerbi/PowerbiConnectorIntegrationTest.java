9da23053435291b7dc73d3bb10b68433b942a6e2
ashalya <ashalya86@gmail.com>
2016-02-26 18:19:57 +0530

    connector version changed

--- src/test/java/org/wso2/carbon/connector/integration/test/powerbi/PowerbiConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/powerbi/PowerbiConnectorIntegrationTest.java
@@ -45,7 +45,7 @@ public class PowerbiConnectorIntegrationTest extends ConnectorIntegrationTestBas
    @BeforeClass(alwaysRun = true)
    public void setEnvironment() throws Exception {
 
-      init("powerbi-connector-1.0.0");
+      init("powerbi-connector-1.0.1-SNAPSHOT");
 
       esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
       esbRequestHeadersMap.put("Content-Type", "application/json");
